9cfff06877b5f3d4fb967ece93ef609ee7f78bef
TechClubPro <62166836+TechClubPro@users.noreply.github.com>
2021-05-07 12:34:17 +0530

    Add files via upload

---
+++ SpeechToText.py
@@ -0,0 +1,39 @@
+"""Program to which takes voice Input. 
+This program uses speech_recognition library.
+This library reads the voice commands from the mic. 
+and converts voice to Text message.
+
+"""
+
+#Using speech_recognition library
+import speech_recognition as speech
+
+
+while True:
+    try:
+        #Take the user input to activate reception of Voice Commands
+        userInput=input("Press v to start voice commands and q to quit")    
+        
+        if userInput=='v':
+            
+            #Take Voice commands from mic
+            r=speech.Recognizer()
+            with speech.Microphone() as source:
+                r.adjust_for_ambient_noise(source)
+                print("Speak:")
+                audio=r.listen(source)
+            #Convert Voice Commands to Text
+            command=r.recognize_google(audio)
+            
+            print("You said: "+command)
+        else:
+            break
+   
+    #Statements to Handle errors while receiving voice commands
+    except speech.UnknownValueError:
+        print("Could not understand audio")
+    except speech.RequestError as e:
+        print("Could not request results; {0}".format(e))
+    except KeyboardInterrupt:
+        break
+print("Nice interacting with you!!")
